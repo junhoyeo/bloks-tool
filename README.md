5b07b106fba6bb1847e6b07c662874b731bc5814
firtoz <108406948+firtoz@users.noreply.github.com>
2023-08-28 22:59:45 +0100

    Add code

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # bloks-parser
 
-Hello world!
+Npm package for parsing Instagram's BLOCKS format.
+
+Uses peggy to generate a parser from the grammar in `src/blok-parser.peggy`.
+
+## Usage
+
+Install the package:
+
+```sh
+yarn add bloks-parser
+```
+
+```ts
+import {parseBloks, visitBloks} from "blok-parser";
+
+const bloks = parseBloks(rawBloks);
+
+visitBloks(bloks, (blok) => {
+    const [funcName, ...args] = blok;
+
+    console.log({
+        funcName,
+        args,
+    });
+
+    return false;
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world!');
+import {BlokExpression, parse, TailPart} from "./generated/blok-parser.peggy";
+
+export const parseBloks = parse;
+
+export const IsBlokExpression = (blok: TailPart): blok is BlokExpression => {
+    return (Array.isArray(blok)
+        && blok.length > 0
+        && typeof blok[0] === 'string'
+    );
+}
+
+export const visitBloks = (node: TailPart, visitor: (blok: BlokExpression) => boolean) => {
+    if (IsBlokExpression(node)) {
+        if (visitor(node)) {
+            return true;
+        }
+
+        for (const arg of node.slice(1)) {
+            if (visitBloks(arg, visitor)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
